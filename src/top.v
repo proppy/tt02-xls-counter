5d6dea1b51add2cf07e90b98db459c2f204f971b
Johan Euphrosine <proppy@google.com>
2022-11-15 01:30:40 +0900

    src/top: update output port

--- src/top.v
+++ src/top.v
@@ -1,8 +1,8 @@
 module top(
   input wire [7:0] io_in,
-  output wire [7:0] out
+  output wire [7:0] io_out
 );
-   wire 	    rdy = 1;
-   wire 	    vld;
-   user_module counter0(io_in[0], io_in[1], rdy, out, vld);
+  wire 	    rdy = 1;
+  wire 	    vld;
+  user_module counter0(io_in[0], io_in[1], rdy, io_out, vld);
 endmodule
