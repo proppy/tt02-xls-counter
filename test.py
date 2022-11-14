4c470f7a6a277041e7bce9828fc869b1693964b3
Johan Euphrosine <proppy@google.com>
2022-11-14 23:58:57 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,46 @@
+import cocotb
+from cocotb.triggers import Timer
+
+
+@cocotb.test()
+async def test_test(dut):
+    # rst 0
+    dut.io_in[1].value = 1
+    dut.io_in[0].value = 1
+    await Timer(1, units='ns')
+    dut.io_in[0].value = 0
+    await Timer(1, units='ns')
+
+    # tick
+    dut.io_in[1].value = 0
+    dut.io_in[0].value = 1
+    await Timer(1, units='ns')
+    dut.io_in[0].value = 0
+    await Timer(1, units='ns')
+
+    assert dut.out.value == 0
+
+    # one more tick
+    dut.io_in[0].value = 1
+    await Timer(1, units='ns')
+    dut.io_in[0].value = 0
+    await Timer(1, units='ns')
+
+    assert dut.out.value == 1
+
+    # even more ticks
+    for i in range(254):
+        dut.io_in[0].value = 1
+        await Timer(1, units='ns')
+        dut.io_in[0].value = 0
+        await Timer(1, units='ns')
+
+    assert dut.out.value == 255
+
+    # the last tick
+    dut.io_in[0].value = 1
+    await Timer(1, units='ns')
+    dut.io_in[0].value = 0
+    await Timer(1, units='ns')
+
+    assert dut.out.value == 0
